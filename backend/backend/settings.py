8736375b8fd7400e6b88974e3a4504ddb687b622
raphael-mottelet <72501205+raphael-mottelet@users.noreply.github.com>
2024-06-27 14:43:35 +0200

    front and backend bug to resolve, pushing working front

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -82,25 +82,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # settings.py
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('POSTGRES_HOST'),
-        'PORT': os.environ.get('POSTGRES_PORT'),
-    }
-}
 
-'''
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-'''
 
 
 # Password validation
