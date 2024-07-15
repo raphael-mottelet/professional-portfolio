25420a90c38bb6317eb9aa9546720a3aff98ad3c
Debian <debian@vps-4aff30e3.vps.ovh.net>
2024-07-15 22:19:37 +0000

    modifying date fields to simple chafields

--- backend/professional_portfolio_app/models.py
+++ backend/professional_portfolio_app/models.py
@@ -20,7 +20,7 @@ class Experience(models.Model):
         (ONGOING, 'Ongoing'),
         (TERMINATED, 'Terminated'),
     ]
-    date = models.DateField()
+    date = models.CharField(max_length=50)
     imageSrc = models.ImageField(upload_to='experience_images/', default='default_image.jpg')
     title = models.CharField(max_length=255)
     location = models.CharField(max_length=100, default='')
